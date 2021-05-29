6b7bcced048e96dd40245072aa05924d5375dc89
dvillaunal <dvilla@unal.edu.co>
2021-05-28 22:23:21 -0500

    .

--- main.r
+++ main.r
@@ -1,4 +1,4 @@
-## ---- eval=FALSE, include=TRUE-----------------------------------------------------
+## ---- eval=FALSE, include=TRUE----------------------------------------------------
 ## "Protocolo:
 ## 
 ##  1. Daniel Felipe Villa Rengifo
@@ -11,24 +11,26 @@
 ##     https://drive.google.com/drive/u/1/folders/1QfvE0DLXE5NQ3kJGEf0F7NZwUx8odv8N"
 
 
-## ----------------------------------------------------------------------------------
+## ---------------------------------------------------------------------------------
 #install.packages("tidyverse")
 #library(tidyverse)
 
 
-## ----------------------------------------------------------------------------------
+## ---------------------------------------------------------------------------------
+# Leemos la base de datos:
 palabras <- read.table(file = "ListaPalabras_v20200520.txt",
                        header = F, stringsAsFactors = F)
 
-
-isograma1.0 <- function(x){
+# Creamos la función para ver si es isograma según un numero dado de palabras:
+isograma1.0 <- function(x, num){
+  "Identifica isogramas, dando un dataframe (de una columna) de palabras y un [num] = que el el numero de palabras de la lista a identificar, (el numero no debe ser mayor a 267751)"
+  "No tomar un numero muy grande para que el sistema no haga bug"
   r <- 0
-  iso <- c()
-  #ct <- 0
   t <- 0
-  while (t < 23) {
+  norep <- c()
+  while (t <= num) {
+    t <- t + 1
     for(i in x){
-      t <- t + 1
       sepa <- as.vector(strsplit(i, "")[[t]])
       q <- data.frame(table(sepa))$Freq
       ct <- 0
@@ -37,20 +39,59 @@ isograma1.0 <- function(x){
           ct <- ct + 1
         }
       }
+      r <- r + 1
       if(ct == length(sepa)){
-        r <- r + 1
-        iso <- c(iso, r)
+        norep <- c(norep, i[r])
       }
     }
   }
-  return(iso)
+  norep <- data.frame("Isogramas" = norep)
+  return(norep)
 }
 
 
-## ----------------------------------------------------------------------------------
-isograma1.0(palabras)
+## ---------------------------------------------------------------------------------
+# Ahora por cuestiones de la memoria del sistema analizaremos los primeros 200 palabras según el orden del data.frame a ver si hay isogramas:
 
-## ---- eval=FALSE, include=FALSE----------------------------------------------------
-## library(knitr)
-## purl("Dia_70_25mayo.Rmd", "main.r")
+isograma <- isograma1.0(palabras, 200)
+isograma$Isogramas <- as.character(isograma$Isogramas)
 
+
+## ---------------------------------------------------------------------------------
+# Ahora exportamos el resultado por orden alfabletico:
+isograma[order(isograma$Isogramas),]
+
+#y exportamos:
+write.table(isograma, file = "IsogramaAlfa.txt", row.names = F)
+ 
+# Ahora a mirar el conteo de palabras:
+
+conteo <- function(x){
+  "Realza un conteo palabra por palabra del vector de isogramas, para sacar cuantas letras tiene cada palabra"
+  t <- 0
+  l <- as.integer(length(x[[1]]))
+  numlet <- c()
+  while (t < l) {
+    t <- t + 1
+   for (i in x){
+    sp <- as.vector(strsplit(i, "")[[t]])
+    let <- paste(length(sp)," letras")
+    numlet <- c(numlet, let)
+   } 
+  }
+  return(numlet)
+}
+
+# Anadimos el vector que genera la funcion al data frame:
+isograma$NumLetras <- conteo(isograma)
+
+# lo volvemos factor y sacamos el conteo:
+isograma$NumLetras <- as.factor(isograma$NumLetras)
+
+# Creamos el data.frame de las frecuencias por numero de letras:
+NumeroDeLetras <- data.frame(table(isograma$NumLetras))
+
+# Renombramos las columnas:
+names(NumeroDeLetras) <- c("# de Letras", "Freq")
+
+print(NumeroDeLetras)
